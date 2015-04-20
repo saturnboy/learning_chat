e35ebd52c6cdb583fb941c6a5777e6ab795409d1
Justin Shacklette <justin@saturnboy.com>
2015-04-20 11:22:13 -0600

    working webapp

--- chat.php
+++ chat.php
@@ -3,18 +3,26 @@ $fp = fopen('msgs.txt', 'a+') or die('no chat log');
 
 if (flock($fp, LOCK_EX)) {  // acquire lock
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        //on GET, read chat log and return it
+        //on GET
         $msgs = array();
+        
+        //read log file
         while($msg = fgets($fp)) {
             $i = strpos($msg, '|');
             if ($i) {
-                $msgs[] = array( 'name' => substr($msg, 0, $i), 'msg' => trim(substr($msg, $i+1)) );
+                $msgs[] = array( 'name' => substr($msg, 0, $i), 'msg' => substr($msg, $i+1) );
             }
         }
+        
+        //return log
+        header('Content-Type: application/json');
         echo json_encode($msgs);
     } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //on POST, write msg to chat log
+        //on POST
         if (isset($_POST) && isset($_POST['name']) && isset($_POST['msg'])) {
+        	//validation goes here...
+        	
+        	//write msg to log file
             fwrite($fp, $_POST['name'] . '|' . $_POST['msg'] . "\n");
             fflush($fp);
         } else {
